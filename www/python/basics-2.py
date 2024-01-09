ffec628bd807477a3e6d15f9b30fc7359ce7df7d
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-09 14:28:58 +0000

    Add comments

--- www/python/basics-2.py
+++ www/python/basics-2.py
@@ -1,4 +1,6 @@
+# Get user inputs
 num1 = float(input("Input the first number: "))
 num2 = float(input("Input the second number: "))
 
+# Print out the division
 print("The division of the first number and the second number is: " + str(num1 / num2))
