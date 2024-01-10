43f9ac8b694067b1ae54b61b88b1f1db46ca8f27
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-10 11:57:46 +0000

    Add comments

--- www/python/collections-1.py
+++ www/python/collections-1.py
@@ -1,2 +1,11 @@
+# Create list of numbers
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Print sum of the list
 print(sum(num))
+
+# Using for loop to sum up the numbers in the list
+
+# sum = 0
+# for i in num:
+#     sum += i
+# print(sum)
