1eb211a96b73fbdd3e6baaa799b8538b9987c455
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-10 12:09:40 +0000

    Activity completed

---
+++ www/python/collections-3.py
@@ -0,0 +1,21 @@
+# Create a list of numbers
+num = [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+# Print out max and min number
+print("Maximum value:", max(num))
+print("Minimum value:", min(num))
+
+# Using for loop for the activity
+
+# max = num[0]
+# min = num[0]
+
+# for i in num:
+#     if max < i:
+#         max = i
+
+#     if min > i:
+#         min = i
+
+# print("Maximum value:", max)
+# print("Minimum value:", min)
