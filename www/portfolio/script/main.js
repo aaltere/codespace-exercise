11d270ebcd65e17300c8a1a49efb048a5f986bbf
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-23 13:37:40 +0000

    Add hero section
    List of changes
    - Add hero image
    - Add button and title for hero image
    - Add margin top with transition
    - Change transiton timing

--- www/portfolio/script/main.js
+++ www/portfolio/script/main.js
@@ -1,13 +1,16 @@
 const brand = document.getElementById("brand");
+const heroImage = document.getElementById("landing-img");
 
 window.addEventListener("scroll", () =>
 {
     if (window.scrollY > 0)
     {
         brand.style.transform = "scale(1.5) translate(0, 0)";
+        heroImage.style.marginTop = "0";
     }
     else if (window.scrollY === 0)
     {
         brand.style.transform = "scale(10) translate(0, 10px)";
+        heroImage.style.marginTop = "20vh";
     }
 });
